127b54a5328bedc36b29c20dda30b228e78ebc30
Shirolist <130971087+Shirolist@users.noreply.github.com>
2023-04-26 21:37:14 +0800

    Add files via upload

--- EFT.py
+++ EFT.py
@@ -1,5 +1,6 @@
 import json
 import tkinter as tk
+from pathlib import Path
 from tkinter import ttk
 from PIL import Image, ImageTk
 
@@ -11,13 +12,8 @@ try:
         materials = json.load(file)
 except FileNotFoundError:
     materials = {
-        '42nd': {'quantity': 1, 'type': 'quest'},
-        'Apollo': {'quantity': 0, 'type': 'quest'},
-        'Aramid': {'quantity': 5, 'type': 'quest'},
-        'Book': {'quantity': 1, 'type': 'quest'},
-        'BEAR Buddy': {'quantity': 1, 'type': 'quest'},
-        'Buckwheat':{'quantity': 0, 'type': 'quest'},
-        'DrLupos':{'quantity': 1, 'type': 'quest'}
+        '42nd': {'quantity': 1},
+
     }
 
 # Initialize the GUI
@@ -37,27 +33,45 @@ def update_material():
 def filtered_materials():
     global filtered_frame  # Declare filtered_frame as a global variable
     material_type = material_type_var.get()
-    filtered_materials = [material for material in materials if materials[material]['type'] == material_type]
+    filtered_materials = [material for material in materials]
     filtered_frame.destroy()
     filtered_frame = tk.Frame(canvas)
     filtered_frame.pack(fill="both", expand=True)
     canvas.create_window((0, 0), window=filtered_frame, anchor="nw")
     
+    # set the attribute for the variable 
+    item_a = tk.Frame(filtered_frame, height=50)
+    item_a.pack(fill="x")
+    label_a = tk.Label(item_a, text="Icon",width = 7)
+    label_a.pack(side="left")
+    name_label_a = tk.Label(item_a, text="Material", width=20)
+    name_label_a.pack(side="left")
+    quantity_label_a = tk.Label(item_a, text="Quantity", width=10)
+    quantity_label_a.pack(side="right")
+
     for material in filtered_materials:
-        image = Image.open(f"{material}.png")
-        image = image.resize((50, 50))
-        photo = ImageTk.PhotoImage(image)
         
+        image = Image.open(f"item_image/{material}.png")
+        image = image.resize((50, 50))
+        
+        photo = ImageTk.PhotoImage(image)
+
         # Create a custom listbox item with the image and material name and quantity
         item = tk.Frame(filtered_frame, height=50)
         item.pack(fill="x")
-        item.photo = photo  # Store the photo as an attribute of the item to prevent it from being garbage collected
+        item.photo = photo  
+
+        # Store the photo as an attribute of the item to prevent it from being garbage collected
         label = tk.Label(item, image=photo)
         label.pack(side="left")
+
         name_label = tk.Label(item, text=material, width=20)
         name_label.pack(side="left")
+
         quantity_label = tk.Label(item, text=f"{materials[material]['quantity']}", width=10)
         quantity_label.pack(side="right")
+        
+
 
 # Create a frame for the materials list and filter
 list_frame = ttk.Frame(root, padding=10)
@@ -69,8 +83,6 @@ scrollbar.pack(side="right", fill="y")
 canvas = tk.Canvas(list_frame, yscrollcommand=scrollbar.set, height=100)
 canvas.pack(side="left", fill="both", expand=True)
 scrollbar.config(command=canvas.yview)
-
-
 
 
 # Create a frame to hold the filtered materials
@@ -95,7 +107,7 @@ material_entry = ttk.Entry(control_frame)
 material_entry.pack()
 
 # Create a label and entry for the quantity to subtract
-quantity_label = ttk.Label(control_frame, text="Quantity:")
+quantity_label = ttk.Label(control_frame, text="You have how many:")
 quantity_label.pack()
 quantity_entry = ttk.Entry(control_frame)
 quantity_entry.pack()
@@ -105,45 +117,9 @@ update_button = ttk.Button(control_frame, text="Update", command=update_material
 update_button.pack(pady=10)
 
 # Create a label and dropdown for the material type filter
-material_type_label = ttk.Label(control_frame, text="Filter by type:")
-material_type_label.pack()
 material_type_var = tk.StringVar()
 material_type_var.set("quest")
-material_type_dropdown = ttk.OptionMenu(control_frame, material_type_var, "quest", "quest", "hideout", command=filtered_materials)
-material_type_dropdown.pack()
 
-'''
-# Create a frame for the material types and attributes display
-display_frame = ttk.Frame(root, padding=10)
-display_frame.pack(side="top", fill="x")    
-
-# Create labels for material types and attributes
-material_type_label = ttk.Label(display_frame, text="Material Type:")
-material_type_label.pack(side="left", padx=10)
-material_type_value = ttk.Label(display_frame, text="")
-material_type_value.pack(side="left")
-
-attribute_label = ttk.Label(display_frame, text="Attribute:")
-attribute_label.pack(side="left", padx=10)
-attribute_value = ttk.Label(display_frame, text="")
-attribute_value.pack(side="left")
-'''
-
-
-'''
-def update_display():
-    # Get the selected material type and attribute
-    material_type = material_type_combo.get()
-    attribute = attribute_combo.get()
-
-    # Update the labels with the selected values
-    material_type_value.config(text=material_type)
-    attribute_value.config(text=attribute)
-
-# Call the update_display function whenever the material type or attribute is changed
-material_type_combo.bind("<<ComboboxSelected>>", lambda event: update_display())
-attribute_combo.bind("<<ComboboxSelected>>", lambda event: update_display())
-'''
 # Filter the materials initially
 filtered_materials()
 
